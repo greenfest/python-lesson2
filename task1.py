afc403cd52c028828e5c3f9e0c026dc674cbff37
greenfest <1greenfest@gmail.com>
2022-10-14 21:17:06 +0500

    edited task1, added task4

--- task1.py
+++ task1.py
@@ -4,7 +4,7 @@ def getFactorials(n):
     arrFactorials = []
     for num in range(1, n + 1):
         arrFactorials.append(math.factorial(num))
-    return arrFactorials;
+    return arrFactorials
 
 n = int(input("Введите число N: "))
 arrFactorials = getFactorials(n)
